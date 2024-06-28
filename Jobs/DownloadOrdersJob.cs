59ff01d95b569666e8320328690d57165ca9e40e
Paul Stan <Paul Stan>
2024-06-28 20:19:41 +0300

    Update DownloadOrdersJob.cs

--- Jobs/DownloadOrdersJob.cs
+++ Jobs/DownloadOrdersJob.cs
@@ -38,20 +38,20 @@ namespace IPBSyncAppNetCore.Jobs
                 {
                     Logger.Info($"Sending order to WME...");
                     bool imported = false;
-                    //aici 
-                    //if (Config.IsDebug)
-                    //{
-                    //    imported = WriteOrderToFile(OCOrder);
-                    //}
-                    //else
-                    //{
-                    //    imported = SendOrderToWME(OCOrder);
-                    //};
+
+                    if (Config.IsDebug)
+                    {
+                        imported = WriteOrderToFile(OCOrder);
+                    }
+                    else
+                    {
+                        imported = SendOrderToWME(OCOrder);
+                    };
 
                     if (imported)
                     {
                         Logger.Info("Order has been successfully imported into WME");
-                        //MarkOrderAsExported(OCOrder);
+                        MarkOrderAsExported(OCOrder);
                     }
                     
                 }
@@ -99,18 +99,6 @@ namespace IPBSyncAppNetCore.Jobs
             return new JArray();
         }
 
-        private async Task<JArray?> LoadProducts()
-        {
-            //C:\laragon\www\ipb\GetInfoArticole.json
-            using (var responseStream = new FileStream(@"C:\laragon\www\ipb\GetInfoArticole.json", FileMode.Open))
-            using (var reader = new JsonTextReader(new StreamReader(responseStream)))
-            {
-                JObject data = (JObject)JToken.ReadFrom(reader);
-
-                return data["InfoArticole"] as JArray;
-            }
-        }
-
         private async Task<OCOrder[]> DownloadOrders()
         {
             // Create an instance of HttpClient
@@ -140,94 +128,6 @@ namespace IPBSyncAppNetCore.Jobs
             }
 
             return Array.Empty<OCOrder>();
-        }
-
-        private async Task CallTransferData()
-        {
-            // Create an instance of HttpClient
-            using var client = new HttpClient();
-
-            // Set base address of the API
-            client.BaseAddress = new Uri(Config.WebRESTAPIURL);
-            client.DefaultRequestHeaders.Add("Authorization", Config.WebAuthorizationToken);
-
-            try
-            {
-                HttpResponseMessage response = await client.PostAsync("transfer-articles", null);
-                var strResponse = await response.Content.ReadAsStringAsync();
-                Logger.Debug("Response from OpenCart");
-                Logger.Debug(strResponse);
-
-                // Check if the response is successful
-                if (response.IsSuccessStatusCode)
-                {
-                    using (var responseStream = await response.Content.ReadAsStreamAsync())
-                    using (var reader = new JsonTextReader(new StreamReader(responseStream)))
-                    {
-                        JObject data = (JObject)JToken.ReadFrom(reader);
-                        Logger.Debug("Received response from OpenCart");
-                        Logger.Debug(data.ToString());
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error("An error appeared when calling truncate endpoint on OpenCart");
-                Logger.Error(ex);
-            }
-        }
-
-        private async Task<string> GetWebAPIResponse(WMEProduct[] products)
-        {
-            // Create an instance of HttpClient
-            using var client = new HttpClient();
-
-            // Set base address of the API
-            client.BaseAddress = new Uri(Config.WebRESTAPIURL);
-            client.DefaultRequestHeaders.Add("Authorization", Config.WebAuthorizationToken);
-
-            try
-            {
-                // Configure JSON serializer options to use PascalCase
-                var options = new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = null
-                };
-
-                var content = JsonContent.Create(products, new MediaTypeWithQualityHeaderValue("application/json"), options);
-
-                // Call the API asynchronously
-                var strContent = await content.ReadAsStringAsync();
-                Logger.Debug("Sending products to OpenCart");
-                Logger.Debug(strContent);
-                HttpResponseMessage response = await client.PostAsync("sync-articles", content);
-                var strResponse = await response.Content.ReadAsStringAsync();
-                Logger.Debug("Response from OpenCart");
-                Logger.Debug(strResponse);
-
-                // Check if the response is successful
-                if (response.IsSuccessStatusCode)
-                {
-                    using (var responseStream = await response.Content.ReadAsStreamAsync())
-                    using (var reader = new JsonTextReader(new StreamReader(responseStream)))
-                    {
-                        JObject data = (JObject)JToken.ReadFrom(reader);
-                        Logger.Debug("Received response from OpenCart");
-                        Logger.Debug(data.ToString());
-                        return data["message"].ToString();
-                    }
-                }
-                else
-                {
-                    return "Products have not been inserted.";
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error("An error appeared when pushing products to OpenCart");
-                Logger.Error(ex);
-                return ex.Message;
-            }
         }
     }
 
