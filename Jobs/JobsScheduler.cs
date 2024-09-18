1acafc2f8cc10df8cc13622a25d534b903c6562e
Paul Stan <Paul Stan>
2024-09-18 20:43:39 +0300

    Update JobsScheduler.cs

--- Jobs/JobsScheduler.cs
+++ Jobs/JobsScheduler.cs
@@ -15,5 +15,25 @@ namespace IPBSyncAppNetCore.Jobs
             RecurringJob.AddOrUpdate<SyncCategoriesJob>("sync-categories", job => job.Execute(), Cron.Daily(0));
             RecurringJob.AddOrUpdate<SyncArticlesJob>("sync-articles", job => job.Execute(), Cron.Daily(2));
         }
+
+        public static void TestScheduledJub(JobBase job)
+        {
+            ClearJob();
+            job.Execute();
+        }
+
+        private static void ClearJob()
+        {
+            RecurringJob.RemoveIfExists("sync-images");
+            RecurringJob.RemoveIfExists("sync-descriptions");
+
+            RecurringJob.RemoveIfExists("download-orders");
+            RecurringJob.RemoveIfExists("sync-stocks");
+
+            RecurringJob.RemoveIfExists("sync-categories");
+            RecurringJob.RemoveIfExists("sync-articles");
+
+
+        }
     }
 }
